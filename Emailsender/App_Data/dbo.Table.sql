e1fde3b12e5f507c3441101f2b37de9197166472
flyerAbhi <139611784+flyerAbhi@users.noreply.github.com>
2023-08-08 11:49:23 +0530

    Delete Emailsender directory

--- Emailsender/App_Data/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table] (
-    [ID]       INT          IDENTITY (1, 1) NOT NULL,
-    [Username] VARCHAR (50) NULL,
-    [Password] VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
